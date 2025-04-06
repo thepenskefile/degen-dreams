e3915bb3edfd243d31db51ecd443a5f8fe52c528
Claudia Nadalin <40446421+thepenskefile@users.noreply.github.com>
2025-04-06 20:19:43 +1000

    feature: add tailwind css

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "@repo/ui/styles";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
