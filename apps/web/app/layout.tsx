6af7042db4f0435a8ef38eaabf8a957bd60f97d9
Claudia Nadalin <40446421+thepenskefile@users.noreply.github.com>
2025-04-06 20:21:21 +1000

    wip

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "@repo/ui/styles";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
