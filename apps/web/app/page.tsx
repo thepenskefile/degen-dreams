3e3c9825ae4e3a66b5947bae4b73f29fa42ba01d
Claudia Nadalin <40446421+thepenskefile@users.noreply.github.com>
2025-04-06 21:19:30 +1000

    feature: button component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,30 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+"use client";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+import { Button } from "@repo/ui/button";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { setTheme } = useTheme();
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Degen Dreams</h1>
-      <Button appName="Degen Dreams">Click me</Button>
+      <h1 className="text-3xl font-bold underline mb-4">Degen Dreams</h1>
+
+      <div className="flex gap-2">
+        <Button
+          onClick={() => {
+            setTheme("dark");
+          }}
+        >
+          Dark mode
+        </Button>
+        <Button
+          onClick={() => {
+            setTheme("light");
+          }}
+        >
+          Light mode
+        </Button>
+      </div>
     </div>
   );
 }
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,32 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes } from "react";
+import { cn } from "./utils";
 
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
-}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({
+  children,
+  className,
+  disabled = false,
+  type = "button",
+  ...props
+}: ButtonProps) => {
   return (
     <button
-      className="bg-blue-500"
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      type={type}
+      disabled={disabled}
+      className={cn(
+        "relative px-4 py-1.5 rounded-lg text-sm transition-all duration-200",
+        "focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 focus:ring-offset-white",
+        "dark:focus:ring-surface-dark dark:focus:ring-offset-2 dark:focus:ring-offset-surface-dark",
+        "border border-[#2E2E2D]",
+        "bg-white hover:bg-gray-100",
+        "dark:bg-surface-dark dark:hover:bg-surface-dark/80",
+        "disabled:opacity-50 disabled:cursor-not-allowed",
+        className
+      )}
+      {...props}
     >
       {children}
     </button>
