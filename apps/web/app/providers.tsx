8b36618cac99bb18cbdbab46f655b0fc7a09fce2
Claudia Nadalin <40446421+thepenskefile@users.noreply.github.com>
2025-04-09 15:04:56 +1000

    feature: shareable link

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         disableTransitionOnChange
       >
         {children}
+        <Toaster />
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
