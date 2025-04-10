ca810cfef34a87ef0ad9537a5438df8d4ba9fd48
Claudia <40446421+thepenskefile@users.noreply.github.com>
2025-04-10 16:53:28 +1000

    feature: default app to dark mode (#19)

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -12,7 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
         attribute="class"
-        defaultTheme="system"
+        defaultTheme="dark"
         enableSystem
         disableTransitionOnChange
       >
